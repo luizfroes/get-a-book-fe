4c682a9739c3c624d961b5f39865993eed1d6b61
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-06 23:55:34 +0000

    refactored app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { SearchBooks } from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
+import { SavedBooks } from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const httpLink = createHttpLink({
